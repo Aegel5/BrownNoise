624749f210b863fded5351da0989a55452b26da1
Alexsander <aegel5@yandex.ru>
2026-01-24 22:17:15 +0300

    Update AudioStreamAutoGen.h

--- src/AudioStreamAutoGen.h
+++ src/AudioStreamAutoGen.h
@@ -1,22 +1,26 @@
 
 using int16 = std::int16_t;
-static const auto v_min = std::numeric_limits<int16>::min();
-static const auto v_max = std::numeric_limits<int16>::max();
+//static const auto v_min = std::numeric_limits<int16>::min();
+//static const auto v_max = std::numeric_limits<int16>::max();
+static const int v_min = -32760;
+static const int v_max = 32760;
 
 
 
 
 namespace Rand {
-
     std::mt19937 m_rng{ std::random_device{}() };        // Быстрый генератор PRNG
-
-    float rand_float(float a, float b) {
-        std::uniform_real_distribution<float> d{ a, b };
+    auto randf(auto a, auto b) {
+        std::uniform_real_distribution<decltype(a)> d( a, b );
         return d(m_rng);
     }
-
-    int16 rand16_s() { std::uniform_int_distribution<int> d(v_min, v_max); return d(m_rng); }
-    int16 rand_int(int a, int b) { std::uniform_int_distribution<int> d(a, b); return d(m_rng); }
+    auto randi(auto a, auto b) {
+        std::uniform_int_distribution<decltype(a)> d(a, b);
+        return d(m_rng);
+    }
+    bool rand_bool() {
+        return m_rng() & 1;
+    }
 }
 
 
@@ -40,8 +44,7 @@ private:
     int m_stepCount = 0;        // Сколько семплов прошло
     const int m_stepsPerChange = 180; // И
 
-    float m_lastOutput = 0;
-    float m_vibratingValue = 0.0f; // Второе состояние для инерции
+    double m_lastOutput = 0;
 
     float smoothStep(float t) {
         return t * t * (3.0f - 2.0f * t);
@@ -49,19 +52,19 @@ private:
 
 public:
 
-    void Gen(int16& smpl) {
+    int Gen() {
         if (mode == Mode::Red1) {
-            bool isUp = Rand::rand_int(0, 1) == 0;
+            bool isUp = Rand::rand_bool();
 
             auto curW = std::max(v_max - prev, prev - v_min);
             auto rem = isUp ? v_max - prev : prev - v_min;
 
-            auto cur = std::round(2.0 * rem / 100);
+            auto cur = std::round(rem / 50.0);
 
             if (isUp) prev += cur;
             else prev -= cur;
-            smpl = prev = std::clamp(prev, (int)v_min, (int)v_max);
-            return;
+
+            return prev = std::clamp(prev, v_min, v_max);
         }
 
         if (mode == Mode::Red2) {
@@ -69,22 +72,21 @@ public:
             //auto cur = (double)rand16_s() / 10;
             auto cur = (double)v_max / 8;
 
-            if (Rand::rand_int(0, 1) == 0) cur = -cur;
+            if (Rand::rand_bool()) cur = -cur;
             if (abs(prev + cur) > v_max) cur = -cur;
             //if ((prev == v_min && cur < 0) || (prev == v_max && cur > 0)) { cur = -cur; }
 
             if ((prev > 0 && cur < 0) || (prev < 0 && cur > 0)) { auto k = 1 + (0.5 * (double)abs(prev) / v_max); cur *= k; }
 
             prev += std::round(cur);
-            smpl = prev = std::clamp(prev, (int)v_min, (int)v_max);
-            return;
+            return prev = std::clamp(prev, v_min, v_max);
         }
 
         if (mode == Mode::Test_La) {
             // Формула шага фазы: 2 * PI * частота / частота_дискретизации
 
            // Генерируем значение от -32767 до 32767
-            smpl = static_cast<int16>(30000 * std::sin(m_phase));
+            auto res = std::round(30000 * std::sin(m_phase));
 
             m_phase += phaseStep;
 
@@ -92,19 +94,18 @@ public:
             if (m_phase > 2.0 * std::numbers::pi)
                 m_phase -= 2.0 * std::numbers::pi;
 
-            return;
+            return res;
         }
 
         if (mode == Mode::White) {
-            smpl = Rand::rand16_s();
-            return;
+            return Rand::randi(v_min, v_max);
         }
 
         if (mode == Mode::Red3) {
             // Если дошли до цели, выбираем новую
             if (m_stepCount >= m_stepsPerChange) {
                 m_previous = m_target;
-                m_target = Rand::rand_float(-30000, 30000);
+                m_target = Rand::randf((float)v_min, (float)v_max);
                 m_stepCount = 0;
             }
 
@@ -114,42 +115,30 @@ public:
             // Плавная интерполяция между точками
             float val = m_previous + (m_target - m_previous) * smoothStep(t);
 
-            smpl = static_cast<int16>(std::clamp(val, -32000.0f, 32000.0f));
             m_stepCount++;
-            return;
+            return std::round(val);
         }
 
         if (mode == Mode::Red4) {
-            // 1. Берем чистый белый шум [-1, 1]
-            float white = Rand::rand_float(-1, 1);
 
-            // 2. Алгоритм интегратора для Brown Noise:
-            // m_lastOutput = (предыдущее * инерция) + (белый_шум * шаг)
-            // Коэффициент 0.995f убирает ВЧ-"песок", делая звук глухим.
-            // Коэффициент 0.01f делает блуждание очень плавным.
-            m_lastOutput = (m_lastOutput * 0.995f) + (white * 0.01f);
+            // мягкий шум - автовозврат через тангенс к 0.
 
-            // 3. Усиливаем до уровня Int16 (32767)
-            // Коричневый шум имеет низкую амплитуду из-за фильтрации, 
-            // поэтому множитель здесь должен быть высоким (около 150-200к).
-            float out = m_lastOutput * 180000.0f;
+            m_lastOutput = (0.997 * m_lastOutput) + (0.02 * Rand::randf(-1.0, 1.0));
+            m_lastOutput = std::tanh(m_lastOutput);
 
-            // 4. Ограничиваем и сохраняем
-            smpl = static_cast<int16>(std::clamp(out, -32000.0f, 32000.0f));
-
-            return;
+            auto res = std::round(m_lastOutput * v_max*2);
+            return res;
         }
 
         if (mode == Mode::Red5) {
-
-
-            return;
         }
+
+        return 0;
     }
 private:
     virtual bool onGetData(Chunk& data) override {
         for (auto& smpl : samples) {
-            Gen(smpl);
+            smpl = Gen();
         }
         data.sampleCount = samples.size();
         data.samples = &samples[0];
