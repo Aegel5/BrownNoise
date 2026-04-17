a99e7a6ad90079e46fa7645ecae672a5327f6eb2
Alexsander <aegel5@yandex.ru>
2026-04-17 15:03:46 +0300

    убираем лишних посредников

--- src/Player.h
+++ src/Player.h
@@ -9,61 +9,70 @@ class Player {
     static constexpr size_t TOTAL_CHUNKS = 256;
 
     ma_device m_device;
-    std::vector<std::array<float, CHUNK_SIZE>> m_chunks;
+    std::vector<std::vector<float>> m_chunks;
     std::atomic<int64_t> m_r{ 0 };
     int64_t m_w = 0;
     float m_volume { 0.1f };
     Red1 red1;
     SineGenerator sin;
 
+    int channels() const { return m_device.playback.channels; }
+
 public:
     void start() {
 
-        step(); // забиваем буфер
 
+        step(); // забиваем буфер
+        ma_device_start(&m_device);
+    }
+    Player() {
         auto config = ma_device_config_init(ma_device_type_playback);
+
         config.playback.format = ma_format_f32;
-        config.playback.channels = 1;
-        config.sampleRate = 48000;
+        //config.playback.channels = 1;
+
         config.periodSizeInFrames = CHUNK_SIZE;
         config.periods = 8;
-        config.noFixedSizedCallback = MA_FALSE;
 
         config.dataCallback = [](ma_device* pD, void* pOut, const void*, ma_uint32 fCount) {
-            if (fCount != CHUNK_SIZE) {
-                std::terminate();
-            }
+
             auto* self = (Player*)pD->pUserData;
             float* out = (float*)pOut;
+
             size_t r = self->m_r.load(std::memory_order_relaxed);
             const float* chunkData = self->m_chunks[r % TOTAL_CHUNKS].data();
-            memcpy(pOut, chunkData, fCount*sizeof(float));
+            memcpy(pOut, chunkData, fCount * sizeof(float) * self->channels());
             self->m_r.store(r + 1, std::memory_order_relaxed);
+
             };
         config.pUserData = this;
 
         ma_device_init(NULL, &config, &m_device);
-        ma_device_start(&m_device);
-    }
-    Player() {
-        m_chunks.resize(TOTAL_CHUNKS);
+
+        m_chunks.resize(TOTAL_CHUNKS, std::vector<float>(CHUNK_SIZE*channels()));
+
     }
 
     ~Player() { ma_device_uninit(&m_device); }
 
     void set_volume(float vol) { 
+
         m_volume = vol; 
         m_w = m_r;
     }
 
 
     void step() {
+        auto ch = channels();
         while (m_w - m_r.load(std::memory_order_relaxed) < TOTAL_CHUNKS) {
             auto& chunk = m_chunks[m_w % TOTAL_CHUNKS];
-            for (auto& smpl : chunk) {
-                smpl = red1.Next() * m_volume;
-                //smpl = sin.Next() * m_volume;
+            for (int64_t i = 0; i < CHUNK_SIZE; i++) {
+                auto cur = red1.Next() * m_volume;
+                for (int64_t j = 0; j < ch; j++) {
+                    chunk[i * ch + j] = cur;
+                }
             }
+
             m_w++;
         }
     }
