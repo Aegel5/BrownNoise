6134bd23b50f5df95798eada8a6a6bb2ae20cd93
Alexsander <aegel5@yandex.ru>
2026-04-20 00:35:00 +0300

    Update Player.h

--- src/Player.h
+++ src/Player.h
@@ -84,7 +84,7 @@ public:
 
         // Фильтр низких частот.
         {
-            auto cfg = ma_hpf_config_init(ma_format_f32, channels(), m_device.sampleRate, 1, 2); // order 1 может не хватить для борьбы с залипанием мембраны.
+            auto cfg = ma_hpf_config_init(ma_format_f32, channels(), m_device.sampleRate, 10, 2); // order 1 может не хватить для борьбы с залипанием мембраны.
             if (ma_hpf_init(&cfg, 0, &hpf)) {
                 std::terminate();
             }
